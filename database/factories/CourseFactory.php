dd30c52d87498f170cb6af8f0ead9534bc6b9acc
farukwebcode21 <farukahmadmk@yahoo.com>
2022-12-19 16:36:47 +0600

    laravel course data complete

---
+++ database/factories/CourseFactory.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Platform;
+use App\Models\User;
+use Doctrine\Inflector\Rules\Word;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Course>
+ */
+class CourseFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            //
+            'name'=>fake()->sentence(),
+            'book'=>rand(0, 1),
+            'price'=>rand(0, 1)?rand(1, 100):0.00,
+            'year'=>rand(2021, 2022),
+            'image'=>fake()->imageUrl(),
+            'description'=>fake()->paragraph(),
+            'link'=>fake()->url(),
+            'submitted_by'=>User::all()->random()->id,
+            'duration'=>rand(0, 2),
+            'platform_id'=>Platform::all()->random()->id,
+        ];
+    }
+}
---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Course;
+use App\Models\Platform;
+use App\Models\Series;
+use App\Models\Topic;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // \App\Models\User::factory(10)->create();
+
+        // \App\Models\User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+
+        $series =['PHP', 'JavaScript', 'WordPress', 'Laravel'];
+        foreach($series as $item){
+            Series::create([
+                'name'=>$item,
+            ]);
+        }
+
+        $topics = ['Eloquent', 'Validation', 'Authentication', 'Testing', 'Refactoring'];
+        foreach($topics as $item){
+            Topic::create([
+                'name'=> $item,
+            ]);
+        }
+        $platforms =['Laracasts', 'Youtube', 'Larajobs', 'Laravel News', 'Laracasts Forum'];
+        foreach($platforms as $item){
+            Platform::create([
+                'name'=>$item,
+            ]);
+        }
+        // create 50 users
+        User::factory(50)->create();
+
+        // create 100 course
+        Course::factory(100)->create();
+    }
+}
